3633363db53420ae0db66ac17d7084919b368432
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-02-27 22:07:16 +0100

    Update slf4j-api from 1.7.25 to 1.7.36 (#16)
    Pull request: https://github.com/domino-osgi/domino-java/pull/16

--- pom.scala
+++ pom.scala
@@ -13,7 +13,7 @@ object Deps {
   val lambdaTest = "de.tototec" % "de.tobiasroeser.lambdatest" % "0.6.2"
   val osgiCompendium = "org.osgi" % "org.osgi.compendium" % "5.0.0"
   val osgiCore = "org.osgi" % "org.osgi.core" % "5.0.0"
-  val slf4j = "org.slf4j" % "slf4j-api" % "1.7.25"
+  val slf4j = "org.slf4j" % "slf4j-api" % "1.7.36"
   val utilsFunctional = "de.tototec" % "de.tototec.utils.functional" % "2.0.1"
   // val felixConfigAdmin = "org.apache.felix" % "org.apache.felix.configadmin" % "1.8.8"
   val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.1.3"
